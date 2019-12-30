00c2d86e0307650be1a9ded334797db2ec7b8eb6
Yuka Saito <yucatio.com@gmail.com>
2019-12-30 13:28:49 +0900

    STEP 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import format from 'date-fns/format'
+import getDate from 'date-fns/getDate'
+import getDay from 'date-fns/getDay'
+import eachDayOfInterval from 'date-fns/eachDayOfInterval'
+import endOfWeek from 'date-fns/endOfWeek'
+import eachWeekOfInterval from 'date-fns/eachWeekOfInterval'
+import startOfMonth from 'date-fns/startOfMonth'
+import endOfMonth from 'date-fns/endOfMonth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getCalendarArray = date => {
+  const sundays = eachWeekOfInterval({
+    start: startOfMonth(date),
+    end: endOfMonth(date)
+  })
+  return sundays.map(sunday =>
+    eachDayOfInterval({start: sunday, end: endOfWeek(sunday)})
+  )
 }
 
-export default App;
+function App() {
+  const targetDate = new Date()
+  const calendar = getCalendarArray(targetDate)
+
+  return (
+    <div>
+      {format(targetDate, 'y年M月')}
+      <table>
+        <thead>
+          <tr>
+            <th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th>
+          </tr>
+        </thead>
+        <tbody>
+          {calendar.map((weekRow, rowNum) => (
+            <tr key={rowNum}>
+              {weekRow.map(date => (
+                <td key={getDay(date)}>{getDate(date)}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
